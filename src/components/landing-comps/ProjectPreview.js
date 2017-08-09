95fe1048d2441157072ff4d36c78a61583dc0a24
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2017-08-09 18:00:13 -0400

    and we've got a basic handleClick and state

--- src/components/landing-comps/ProjectPreview.js
+++ src/components/landing-comps/ProjectPreview.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class ProjectPreview extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // };
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+      selectedProj: '',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleClick(e) {
+    const id = e.target.id;
+    console.log('projectID', id)
+
+    if (this.state.selected) {
+      this.setState({selectedProj: id})
+    } else {
+      console.log("selected: TRUE")
+      this.setState({
+        selected: true,
+        selectedProj: id,
+      })
+    }
+  }
 
   render() {
     return (
@@ -13,15 +33,32 @@ class ProjectPreview extends Component {
 
         <div className='row'>
           <div className='project col-sm-4'>
-            <img className='project-pic' src='img/grey-placeholder.png' alt='project-1'/>
+            <img
+              id='0_project'
+              className='project-pic'
+              src='img/grey-placeholder.png'
+              alt='project-1'
+              onClick={(e) => this.handleClick(e)}/>
             <p className='project-title'>PROJECT</p>
           </div>
           <div className='project col-sm-4'>
-            <img className='project-pic' src='img/grey-placeholder.png' alt='project-2'/>
+            <img
+              id='1_project'
+              className='project-pic'
+              src='img/grey-placeholder.png'
+              alt='project-2'
+              onClick={(e) => this.handleClick(e)}
+            />
             <p className='project-title'>PROJECT</p>
           </div>
           <div className='project col-sm-4'>
-            <img className='project-pic' src='img/grey-placeholder.png' alt='project-3'/>
+            <img
+              id='2_project'
+              className='project-pic'
+              src='img/grey-placeholder.png'
+              alt='project-3'
+              onClick={(e) => this.handleClick(e)}
+            />
             <p className='project-title'>PROJECT</p>
           </div>
         </div>
